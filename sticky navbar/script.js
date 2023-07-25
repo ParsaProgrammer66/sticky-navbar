7200e48ada78b0f73d02ed8162721e8f15512d81
parsa farjami <parsafarjami138701@gmail.com>
2023-07-25 20:02:18 +0330

    sticky navbar

---
+++ sticky navbar/script.js
@@ -0,0 +1,10 @@
+const navbarEl=document.querySelector(".navbar");
+const bottomcontainerEl=document.querySelector(".bottom-container");
+window.addEventListener("scroll",()=>{
+    if(window.scrollY > bottomcontainerEl.offsetTop-navbarEl.offsetHeight-50){
+        navbarEl.classList.add("active");
+    }
+    else{
+        navbarEl.classList.remove("active");
+    }
+});
